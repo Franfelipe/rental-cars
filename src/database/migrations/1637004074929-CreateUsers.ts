667e6e0b18da38eb5e2892b28a073841fa47ad2e
francisco felipe riffel <francisco.riffel@unochapeco.edu.br>
2021-11-15 16:36:50 -0300

    user migration e entitie

---
+++ src/database/migrations/1637004074929-CreateUsers.ts
@@ -0,0 +1,54 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1637004074929 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name:"users",
+            columns:[
+                {
+                    name:"id",
+                    type:"uuid",
+                    isPrimary:true
+                },
+                {
+                    name:"name",
+                    type:"varchar",
+                    isUnique:true
+                },
+                {
+                    name:"username",
+                    type:"varchar"
+                },
+                {
+                    name:"password",
+                    type:"varchar"
+                },
+                {
+                    name:"email",
+                    type:"varchar",
+                    isUnique:true
+                },
+                {
+                    name:"driver_license",
+                    type:"varchar"
+                },
+                {
+                    name:"idAdmin",
+                    type:"boolean",
+                    default:false
+                },
+                {
+                    name:"created_at",
+                    type:"timestamp",
+                    default:"now()"
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
